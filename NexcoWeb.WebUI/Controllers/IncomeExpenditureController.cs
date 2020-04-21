f35b0ac10a6bab011c37104f8fecde1199ba8785
Manea Alexandru <sasedimineata@yahoo.com>
2020-04-21 01:42:47 +0100

    add linq

---
+++ NexcoWeb.WebUI/Controllers/IncomeExpenditureController.cs
@@ -0,0 +1,35 @@
+﻿using NexcoWeb.Domain.Concrete;
+using NexcoWeb.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NexcoWeb.WebUI.Controllers
+{
+    public class IncomeExpenditureController : Controller
+    {
+        // GET: IncomeExpenditure
+        public ActionResult Index()
+        {
+            EFDbContext db = new EFDbContext();
+            List<Income> incomes = db.Incomes.ToList();
+            List<Expenditure> expenditures = db.Expenditures.ToList();
+            var query = from i in incomes
+                        join ex in expenditures on i.IncomeAddedOn equals ex.ExpensesAddedOn
+                        select new Budget { Income = i, Expenditure = ex };
+            return View(query);
+        }
+        public ActionResult Statistics()
+        {
+            EFDbContext db = new EFDbContext();
+            List<Income> incomes = db.Incomes.ToList();
+            List<Expenditure> expenditures = db.Expenditures.ToList();
+            var result = from i in incomes
+                        join ex in expenditures on i.IncomeAddedOn equals ex.ExpensesAddedOn
+                        select new Budget { Income = i, Expenditure = ex };
+            return View(result);
+        }
+    }
+}
---
+++ NexcoWeb.WebUI/Controllers/JoinTablesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NexcoWeb.WebUI.Models;
+using System.Net.Http;
+
+namespace NexcoWeb.WebUI.Controllers
+{
+    public class JoinTablesController : Controller
+    {
+        // GET: JoinTables
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
