45bb6b89e0b6bd8a97e9d7822390cdfc6d168c9b
Manea Alexandru <sasedimineata@yahoo.com>
2020-04-25 11:14:06 +0100

    chaneges

--- NexcoWeb.WebUI/Controllers/AdminBudgetController.cs
+++ NexcoWeb.WebUI/Controllers/AdminBudgetController.cs
@@ -32,60 +32,6 @@ namespace NexcoWeb.WebUI.Controllers
             return View(repositoryBudget.Budgets.OrderByDescending(p => p.BudgetId));
         }
 
-        public ViewResult Create()
-        {
-            return View(new Budget());
-        }
-        [HttpPost]
-        public ActionResult Create (Budget budget)
-        {
-            if (ModelState.IsValid)
-            {
-                repositoryBudget.SaveBudget(budget);
-                TempData["message"] = string.Format("{0} has been added",
-                    budget.DescriptionBudget);
-                return RedirectToAction("../Budget/List");
-            }
-            else
-            {
-                return View(budget);
-            }
-        }
-
-        public ViewResult EditBudget(int BudgetId)
-        {
-            Budget budget = repositoryBudget.Budgets.FirstOrDefault
-                
-                (p => p.BudgetId == BudgetId);
-            return View(budget);
-        }
-
-        [HttpPost]
-        public ActionResult EditBudget(Budget budget)
-        {
-            if (ModelState.IsValid)
-            {
-                repositoryBudget.SaveBudget(budget);
-                TempData["message"] = string.Format("{0} has been saved",
-                    budget.DescriptionBudget);
-                return RedirectToAction("IndexBudget");
-            }
-            else
-            {
-                return View(budget);
-            }
-        }
-
-        [HttpPost]
-        public ActionResult DeleteBudget(int BudgetId)
-        {
-            Budget deletedBudget = repositoryBudget.DeleteBudget(BudgetId);
-            if (deletedBudget != null)
-            {
-                TempData["message"] = string.Format("{0} was deleted",
-                    deletedBudget.DescriptionBudget);
-            }
-            return RedirectToAction("IndexBudget");
-        }
+     
     }
 }
